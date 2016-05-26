fccf1d60e7df8fd0f3b5c586c07ce81f933c2512
Kevin Bowrin <kevin.bowrin@carleton.ca>
2016-05-26 13:10:09 -0400

    Added person processors

---
+++ person_check.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import click
+import collections
+import csv
+import os
+
+Person = collections.namedtuple('Person', 'familyname, givenname, link')
+
+@click.command()
+@click.argument('filename', nargs=1, type=click.File('r'))
+def check_people(filename):
+    """Process an artudis Person json file. 
+       Check for people without work url, identifiers, work affiliation, name variant."""
+
+    without_work_url = []
+    without_identifiers = []  
+    without_work_affiliation = []
+    without_name_variant = []
+
+    for line in filename:
+        json_person = json.loads(line)
+        person = Person(json_person["family_name"], json_person["given_name"], "https://carleton.artudis.com/ppl/{}/".format(json_person["__id__"])) 
+        
+        without_work_url.append(person)
+        if json_person['contact'] != []:
+            for contact in json_person['contact']:
+                if contact['role'] == u"work" and isinstance(contact['website'], basestring) and len(contact['website']) > 1:
+                    without_work_url.pop()
+                    break
+
+        without_identifiers.append(person)
+        if json_person['identifier'] != []:
+            without_identifiers.pop()
+
+        without_work_affiliation.append(person)
+        if json_person['affiliation'] != []:
+            for affiliation in json_person['affiliation']:
+                if affiliation['role'] == u"workRelation":
+                    without_work_affiliation.pop()
+                    break
+
+        without_name_variant.append(person)
+        if json_person['name_info'] != []:
+            for name_info in json_person['name_info']:
+                if name_info['type'] == u"alternative":
+                    without_name_variant.pop()
+                    break
+
+    click.echo("Number of people without work urls: {}".format(len(without_work_url)))
+    save_to_csv(without_work_url, filename, "without_work_url")    
+
+    click.echo("Number of people without identifiers: {}".format(len(without_identifiers)))
+    save_to_csv(without_identifiers, filename, "without_identifiers")
+
+    click.echo("Number of people without work affiliations: {}".format(len(without_work_affiliation)))
+    save_to_csv(without_work_affiliation, filename, "without_work_affiliation")
+
+    click.echo("Number of people without name variants: {}".format(len(without_name_variant)))
+    save_to_csv(without_name_variant, filename, "without_name_variant")
+
+def save_to_csv(listofpeople, filename, name):
+
+    csv_filename = "{}_{}.csv".format(os.path.splitext(filename.name)[0], name)
+    with open(csv_filename, 'wb') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['familyname', 'givenname', 'link'])
+        for person in listofpeople:
+            csvwriter.writerow([person.familyname.encode('utf-8'), person.givenname.encode('utf-8'), person.link.encode('utf-8')])
+    click.echo("Saved to {}".format(csv_filename))
+
+if __name__ == '__main__':
+    check_people()
---
+++ person_name_duplicates.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import click
+import collections
+import csv
+import os
+import Levenshtein
+
+Person = collections.namedtuple('Person', 'familyname, givenname, link')
+
+@click.command()
+@click.argument('filename', nargs=1, type=click.File('r'))
+def check_names(filename):
+    """Process an artudis Person json file. 
+       Check for people with duplicate names."""
+
+    people = []     
+    processed = set([])
+
+    for line in filename:
+        json_person = json.loads(line)
+        person = Person(json_person["family_name"], json_person["given_name"], "https://carleton.artudis.com/ppl/{}/".format(json_person["__id__"])) 
+        people.append(person)   
+
+    csv_filename = "{}_{}.csv".format(os.path.splitext(filename.name)[0], 'potential_duplicates')
+    with open(csv_filename, 'wb') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['familyname1', 'givenname1', 'link1', 'familyname2', 'givenname2', 'link2', 'ratiofamilyname', 'ratiogivenname'])
+
+        for person1 in people:
+            for person2 in people:
+                if person1.link != person2.link:
+                    link_pair = tuple(sorted([person1.link, person2.link]))
+                    if link_pair not in processed:
+                        familyname_ratio = Levenshtein.ratio(person1.familyname, person2.familyname)
+                        givenname_ratio = Levenshtein.ratio(person1.givenname, person2.givenname)
+                        if familyname_ratio > 0.7 and givenname_ratio > 0.4:
+                            click.echo("{}, {}, {} {}".format(person1, person2, familyname_ratio, givenname_ratio))
+                            csvwriter.writerow([person1.familyname.encode('utf-8'), person1.givenname.encode('utf-8'), person1.link, 
+                                                person2.familyname.encode('utf-8'), person2.givenname.encode('utf-8'), person2.link, 
+                                                familyname_ratio, givenname_ratio])
+                    processed.add(link_pair)
+
+    click.echo("Saved to {}".format(csv_filename))
+
+if __name__ == '__main__':
+    check_names()
